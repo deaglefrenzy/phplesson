e28e26602f90920f93fec67c88058ec335f7cbe7
Suryo <deaglefrenzy@gmail.com>
2024-11-14 16:02:36 +0800

    sampai 25

--- Lesson 23/router.php
+++ Lesson 23/router.php
@@ -1,17 +1,6 @@
 <?php
-$uri = parse_url($_SERVER['REQUEST_URI'])['query'];
-//$uri = $_SERVER['REQUEST_URI'];
-//dd($uri);
 
-$routes =
-    [
-        "" => "controllers/index.php",
-        "about" => "controllers/about.php",
-        "notes" => "controllers/notes.php",
-        "note" => "controllers/note.php",
-        "contact" => "controllers/contact.php"
-    ];
-
+$routes = require('routes.php');
 
 function routeToController($uri, $routes)
 {
@@ -29,5 +18,5 @@ function abort($code = 404)
     require("views/{$code}.php");
     die();
 }
-
+$uri = parse_url($_SERVER['REQUEST_URI'])['query'];
 routeToController($uri, $routes);
