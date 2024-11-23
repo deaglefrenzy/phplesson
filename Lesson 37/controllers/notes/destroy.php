aa3e698ea22fa099715dbd01accba57dc16e7c6b
Suryo <deaglefrenzy@gmail.com>
2024-11-23 13:16:41 +0800

    until 38 working

---
+++ Lesson 37/controllers/notes/destroy.php
@@ -0,0 +1,25 @@
+<?php
+
+use Core\App;
+use Core\Database;
+
+//$config = require base_path('config.php');
+//$db = new Database($config['database']);
+
+$db = App::resolve(Database::class);
+//dd($db);
+
+$currentUserId = 1;
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$db->query('delete from notes where id = :id', [
+    'id' => $_POST['id']
+]);
+
+header('location: ?notes');
+exit();
